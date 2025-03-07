6df6709b2378bab164ab6698107f2437af708100
Anton Pavlov <pavlov@myyahoo.com>
2025-03-07 14:21:45 +0000

    config.py was imported to main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
 from app.database import Base, engine
+from app.config import APP_NAME, DEBUG  # Импортируем настройки
 
-app = FastAPI()
+app = FastAPI(title=APP_NAME, debug=DEBUG)  # Используем переменные из config.py
 
 # Создаём таблицы, если их нет
 Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"message": f"Welcome to {APP_NAME}"}
