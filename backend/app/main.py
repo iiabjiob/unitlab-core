91ed6bf93a62272a14c0390cc445ed61273a450e
Anton Pavlov <pavlov@myyahoo.com>
2025-03-10 17:19:26 +0000

    fastAPI params was changed

--- backend/app/main.py
+++ backend/app/main.py
@@ -9,21 +9,12 @@ async def lifespan(app: FastAPI):
     yield
 
 app = FastAPI(
-    title=settings.APP_NAME,         # Название API
-    description=settings.DESCRIPTION, # Описание API
-    version=settings.VERSION,         # Версия API
-    contact={
-        "name": "Anton Pavlov",
-        "email": "pavlov@myyahoo.com",
-    },  # Контактная информация
-    license_info={
-        "name": "MIT License",
-        "url": "https://opensource.org/licenses/MIT",
-    },  # Лицензия
-    debug=settings.DEBUG,             # Включает режим отладки
-    lifespan=lifespan                 # Управление жизненным циклом
-)
+    title=settings.app_name,
+    description=settings.description,
+    version=settings.version,
+    debug=settings.debug,
+    lifespan=lifespan)
 
 @app.get("/")
 def read_root():
-    return {"message": f"Welcome to {settings.APP_NAME}"}
+    return {"message": f"Welcome to {settings.app_name}"}
