f8368a55b0483d1bb801f227a23881b3a23e1df0
Anton Pavlov <pavlov@myyahoo.com>
2025-03-11 15:54:45 +0000

    bug fixed

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,18 +2,20 @@ from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from app.core.config import get_settings
 
+settings = get_settings()
+
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     yield
 
 app = FastAPI(
-    title=get_settings.app_name,
-    description=get_settings.description,
-    version=get_settings.version,
-    debug=get_settings.debug,
+    title=settings.app_name,
+    description=settings.description,
+    version=settings.version,
+    debug=settings.debug,
     lifespan=lifespan
     )
 
 @app.get("/")
 def read_root():
-    return {"message": f"Welcome to {get_settings.app_name}"}
+    return {"message": f"Welcome to {settings.app_name}"}
