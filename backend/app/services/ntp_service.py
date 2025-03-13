d855ca11f016cf5c03cc192ded31f469242a9ae3
Anton Pavlov <pavlov@myyahoo.com>
2025-03-13 14:16:15 +0000

    chrony was used for ntp with custom servers

--- backend/app/services/ntp_service.py
+++ backend/app/services/ntp_service.py
@@ -7,11 +7,13 @@ settings = get_settings()
 class NTPService:
     """Сервис для управления NTP-серверами"""
 
-    CHRONY_CONFIG_PATH = "/etc/chrony/chrony.conf"
+    CHRONY_SOURCES_DIR = "/etc/chrony/sources.d"
+    CHRONY_SOURCE_FILE = os.path.join(CHRONY_SOURCES_DIR, "fat-simulator-ntp.sources")
 
     @staticmethod
     def apply_ntp_config():
-        """ Генерирует новый конфиг NTP и применяет его """
+        """Обновляет NTP-серверы в `/etc/chrony/sources.d/` и применяет их"""
+
         ntp_server_1 = settings.ntp_server_1
         ntp_server_2 = settings.ntp_server_2
 
@@ -19,23 +21,25 @@ class NTPService:
             server {ntp_server_1} iburst
             server {ntp_server_2} iburst
             """
-        # Проверяем, существует ли конфигурационный файл
-        if not os.path.exists(NTPService.CHRONY_CONFIG_PATH):
-            raise FileNotFoundError(f"File {NTPService.CHRONY_CONFIG_PATH} not fount. Install chrony.")
 
-        # Перезаписываем конфигурационный файл
+        # Проверяем, существует ли `/etc/chrony/sources.d/`
+        if not os.path.exists(NTPService.CHRONY_SOURCES_DIR):
+            raise FileNotFoundError(f"Directory {NTPService.CHRONY_SOURCES_DIR} not found. Install chrony.")
+
+        # Записываем NTP-серверы в `/etc/chrony/sources.d/custom-ntp.sources`
         try:
-            with open(NTPService.CHRONY_CONFIG_PATH, "w") as f:
+            with open(NTPService.CHRONY_SOURCE_FILE, "w") as f:
                 f.write(config)
 
-            subprocess.run(["sudo", "systemctl", "restart", "chrony"], check=True)
-           
+            # Перезагружаем источники времени (без перезапуска `chronyd`)
+            subprocess.run(["sudo", "chronyc", "reload", "sources"], check=True)
+
         except Exception as e:
             raise RuntimeError(f"Ошибка при обновлении NTP-конфигурации: {e}")
 
     @staticmethod
     def get_ntp_servers():
-        """ Возвращает текущие NTP-серверы из .env """
+        """Возвращает текущие NTP-серверы из .env"""
         return {
             "ntp_server_1": settings.ntp_server_1,
             "ntp_server_2": settings.ntp_server_2
