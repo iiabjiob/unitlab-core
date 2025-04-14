1f9f8bd048e2e7c4c8992c4cc833d289a9e6c451
Anton Pavlov <pavlov@myyahoo.com>
2025-04-14 08:58:43 +0100

    setup script was updated

--- installer/install.sh
+++ installer/install.sh
@@ -5,7 +5,12 @@ echo "📦 Starting full installation..."
 
 for script in ./setup/*.sh; do
   echo -e "\n▶️ Executing $script..."
-  bash "$script"
+  
+  if [[ "$(basename "$script")" == "70-wifi-hotspot.sh" ]]; then
+    sudo bash "$script"
+  else
+    bash "$script"
+  fi
 done
 
 echo -e "\n✅ System setup complete!"
