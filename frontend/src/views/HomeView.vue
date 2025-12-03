ef51fc47b832d54ca3e86d5c08b08c6dff05c896
Anton Pavlov <pavlov@myyahoo.com>
2025-12-03 05:29:35 +0000

    envirement was prepeared trow dev container

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="p-3">
 
-
   </div>
 </template>
 
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,6 +8,9 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vite.dev/config/
 export default defineConfig({
   server: {
+    host: true,
+    port: 5173,
+    strictPort: true,
     proxy: {
       '/api': {
         target: 'http://localhost:8000',
